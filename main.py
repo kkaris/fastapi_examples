fbd9352cd67bd26a6ce3fb921c30d88a8fd495eb
kkaris <karis.klas@gmail.com>
2020-11-11 12:01:24 -0500

    Add some typing and do more functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from typing import List, Set, Tuple, Dict
 
 app = FastAPI()
+
 
 @app.get('/')
 async def root():
@@ -10,3 +12,18 @@ async def root():
 @app.post('/items/{item_id}')
 async def read_item(item_id: int):
     return {'item_id': item_id}
+
+
+def list_func(items: List[str]):
+    print(', '.join(items))
+    for item in items:
+        print(item.capitalize())
+
+
+def tuple_set_func(tuple_items: Tuple[int, str], set_items: Set[bytes]):
+    return tuple_items, set_items
+
+
+def dict_func(prices: Dict[str, float], ):
+    for item, price in prices.items():
+        print(item.capitalize(), str(price))
