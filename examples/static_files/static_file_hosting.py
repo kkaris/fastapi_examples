ad865b362f21648444f6cca48a4b2e8b23cb7bca
kkaris <karis.klas@gmail.com>
2020-12-08 20:58:11 -0500

    App runs

--- examples/static_files/static_file_hosting.py
+++ examples/static_files/static_file_hosting.py
@@ -1,3 +1,17 @@
 """
 Example of providing static files
+
+As always, run with
+> uvicorn module.file:app --reload
 """
+from pathlib import Path
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+DATA_DIR = Path(__file__).parent.joinpath('data')
+print(f'DATA_DIR={DATA_DIR.absolute().as_posix()}')
+
+app = FastAPI()
+
+app.mount('/data', StaticFiles(directory=DATA_DIR.absolute().as_posix()),
+          name='data')
