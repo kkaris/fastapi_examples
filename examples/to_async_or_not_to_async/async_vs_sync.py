6e2ea3d45f2996431a6cf5aaee64586bce3fc5e4
kkaris <karis.klas@gmail.com>
2020-12-17 21:20:04 -0500

    Add examples, remove todos

--- examples/to_async_or_not_to_async/async_vs_sync.py
+++ examples/to_async_or_not_to_async/async_vs_sync.py
@@ -2,7 +2,9 @@
 This example show when to use and when not to use async function definitions
 
 Main gist:
-You can only use `await` inside of functions created with `async def`
+You can only use `await` inside of functions created with `async def`,
+however, you CAN create an async def function without using await inside
+of it (but that doesn't make much sense)
 
 Some reading
 https://fastapi.tiangolo.com/tutorial/dependencies/#to-async-or-not-to-async
@@ -49,8 +51,46 @@ Copied from fastapi.tiangolo.com/async/:
 
  If you just don't know, use normal def."
 
-Todo:
- - Add example of endpoint that has to be run with just `def`
- - Add example of endpoint where it doesn't matter
- - Add example of endpoint where `async def` should/must be used
 """
+import requests
+import aiofiles
+from typing import Optional
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+async def read_file_async(fname: str):
+    """Reads a file asynchronously"""
+    async with aiofiles.open(fname) as f:
+        contents = f.read()
+    return contents
+
+
+# This path operation function does not communicate with anything else,
+# and should therefore be async-ed
+@app.get('/adder')
+async def simple_adder(a: int, b: Optional[int] = 0):
+    """Adds two numbers"""
+    return {'a + b': a + b}
+
+
+# This path operation function calls an external library that doesn't
+# support using await, so define it without async
+@app.get('/status_code')
+def get_page_status_code(q: str = 'fastapi'):
+    """Query duckduckgo.com for a user provided query"""
+    res = requests.get('https://api.duckduckgo.com/',
+                       params={'q': q,
+                               'format': 'json',
+                               'pretty': 1})
+    return {'status_code': res.status_code}
+
+
+# This path operation function calls an external library that does support
+# using await, so define it with async
+@app.get('/file/{fname}')
+async def get_file(fname: str):
+    cont = await read_file_async(fname)
+    return {'content': cont}
