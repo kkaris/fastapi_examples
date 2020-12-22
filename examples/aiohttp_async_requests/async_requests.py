ff51cc384c04812b0aa1816fa74ec5ad18e85444
kkaris <karis.klas@gmail.com>
2020-12-22 10:46:21 -0500

    ERR Make endpoints, functions. Currently errors.

--- examples/aiohttp_async_requests/async_requests.py
+++ examples/aiohttp_async_requests/async_requests.py
@@ -10,8 +10,32 @@ https://github.com/aio-libs/aiohttp
 """
 import aiohttp
 import asyncio
+from typing import Optional, Dict
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
+
+async def search_ddg(search: str) -> Dict:
+    """Search duckduckgo"""
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://api.duckduckgo.com/',
+                               params={'q': search,
+                                       'format': 'json',
+                                       'pretty': 1}) as response:
+            print(f'Status: {response.status}')
+            print(f'Content-type: {response.headers["content-type"]}')
+            json_resp = await response.json()
+
+    return json_resp
+
+
+@app.get('/search_ddg')
+async def search_ddg(search_str: str = 'fastapi'):
+    resp_json = await search_ddg(search_str)
+    return resp_json
+
+
+@app.get('/health')
+async def health():
+    return {'status': 'pass'}
