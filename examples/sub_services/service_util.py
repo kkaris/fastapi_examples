e536810d4c79042cddcd395f998882eecfe36e05
kkaris <karis.klas@gmail.com>
2020-12-22 14:24:32 -0500

    Add BaseModels, update file docstring

--- examples/sub_services/service_util.py
+++ examples/sub_services/service_util.py
@@ -1,35 +1,65 @@
 """
-Add:
-- pydantic BaseModel for defining POST request body, see here:
-  https://fastapi.tiangolo.com/tutorial/body/
-
+This file contains helper functions and basemodels for the services
 """
-from typing import Optional, Union
-from pydantic import BaseModel, validator
+from typing import Optional, Dict, List
+from pydantic import BaseModel
+from indra_depmap_service.util import get_query_hash
 
 
-# __all__ = ['QueryBody', 'QueryBodySum', 'WriteToLogQuery']
-__all__ = ['WriteToLogQuery']
+__all__ = ['NetworkSearchQuery', 'Job', 'JobStatus', 'ServiceStatus',
+           'Edge', 'PathResult', 'QueryResult']
 
 
-# class QueryBody(BaseModel):
-#     source: Optional[str]  # Todo: check if A XOR B possible
-#     target: Optional[str]
-#     sign: Optional[int, str]
-#     max_paths: int
-#
-#     # See https://github.com/samuelcolvin/pydantic/issues/1223
-#     @validator('source', 'target')
-#     def prevent_both_none(self, s, t):
-#         assert s or t, 'At least one of source or target must be provided'
-#         return s, t
+class ServiceStatus(BaseModel):
+    """Service status model"""
+    service_type: str  # Specify unsigned worker, signed worker, master etc
+    status: str  # Specify available or loading or similar
 
 
-# class QueryBodySum(BaseModel):
-#     a: float
-#     b: float
+class NetworkSearchQuery(BaseModel):
+    """Small version of final NetworkSearchQuery"""
+    source: str
+    target: Optional[str]
+    signed: Optional[int] = None
+
+    def get_hash(self):
+        """Get the corresponding query hash of the query"""
+        return get_query_hash(self.dict())
 
 
-class WriteToLogQuery(BaseModel):
-    address: str
-    message: Optional[str]
+class Job(BaseModel):
+    """Defines a job"""
+    status: str
+    id: str
+    query: NetworkSearchQuery
+
+
+class JobStatus(BaseModel):
+    """Jobstatus model providing META data"""
+    status: str
+    id: str
+    fname: Optional[str] = None
+    location: Optional[str] = None  # e.g. URL or s3 path
+
+
+# RESULT MODELS
+class Edge(BaseModel):
+    """The smallest unit of result"""
+    hashes: List[int]
+    sources: List[Dict[str, int]]
+
+
+class PathResult(BaseModel):
+    """Results for directed paths"""
+    one_edge: Optional[List[Edge]] = None
+    two_edge: Optional[List[Edge, Edge]] = None
+
+
+class QueryResult(BaseModel):
+    """The main result model
+
+    All parameters are optional to account for empty
+    """
+    forward_paths: Optional[PathResult] = None
+    backward_paths: Optional[PathResult] = None
+    shared_targets: Optional[List[Edge]] = None
