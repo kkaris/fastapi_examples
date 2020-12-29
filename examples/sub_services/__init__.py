18ae42c073fc908c8102160cd6715cb3bb463ac7
kkaris <karis.klas@gmail.com>
2020-12-28 19:05:46 -0500

    Load environment variables in __init__.py

--- examples/sub_services/__init__.py
+++ examples/sub_services/__init__.py
@@ -10,3 +10,31 @@ else:
 
 if not DATA_DIR.is_dir():
     DATA_DIR.mkdir(parents=True)
+
+
+# Set service configs
+try:
+    WORKER_TYPE = environ['WORKER_TYPE']  # Sets the identity of the service
+    public_port = environ['MAIN_PORT']
+    signed_port = environ['SIGNED_PORT']
+    unsigned_port = environ['UNSIGNED_PORT']
+    SERVICE_PORTS = {'public': f'http://127.0.0.1:{public_port}',
+                     'signed': f'http://127.0.0.1:{signed_port}',
+                     'unsigned': f'http://127.0.0.1:{unsigned_port}'}
+    graph_dir = Path(environ['GRAPH_DIR']).absolute()
+    INDRA_MDG = graph_dir.joinpath('indranet_multi_digraph_latest.pkl')
+    INDRA_DG = graph_dir.joinpath('indranet_dir_graph_latest.pkl')
+    INDRA_SNG = graph_dir.joinpath('indranet_sign_node_graph_latest.pkl')
+    INDRA_SEG = graph_dir.joinpath('indranet_sign_edge_graph_latest.pkl')
+    FILES = {
+        'dir_graph': INDRA_DG.as_posix() if INDRA_DG.is_file() else None,
+        # 'multi_digraph_path': INDRA_MDG_CACHE if path.isfile(INDRA_MDG_CACHE)
+        # else None,
+        'multi_digraph': None,
+        'sign_edge_graph': INDRA_SEG if INDRA_SEG.is_file() else None,
+        'sign_node_graph': INDRA_SNG if INDRA_SNG.is_file() else None
+    }
+except KeyError as err:
+    raise KeyError(
+        'Mandatory environment variable not set in config file'
+    ) from err
