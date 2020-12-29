fd282f38a8c69430a6b835f2eeb131349f3c6db0
kkaris <karis.klas@gmail.com>
2020-12-29 17:04:16 -0500

    WIP Start aioboto3 example

---
+++ examples/async_aws/async_aws.py
@@ -0,0 +1,8 @@
+"""
+Shows how to use the package aioboto3 to run certain boto3 functions async
+See:
+https://pypi.org/project/aioboto3/
+https://aioboto3.readthedocs.io/en/latest/usage.html#s3-examples
+"""
+import asyncio
+import aioboto3
