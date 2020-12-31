7682145e98b5ada55074aae95b5613e066a3404d
kkaris <karis.klas@gmail.com>
2020-12-30 20:06:36 -0500

    Put parenthesis on variable

--- examples/start_signed.sh
+++ examples/start_signed.sh
@@ -1,4 +1,4 @@
 echo "Starting signed service"
 export WORKER_ROLE=SIGNED
-uvicorn sub_services.sub_service:app --port $SIGNED_PORT > access_signed.log &
+uvicorn sub_services.sub_service:app --port "$SIGNED_PORT" > access_signed.log &
 
--- examples/start_unsigned.sh
+++ examples/start_unsigned.sh
@@ -1,4 +1,4 @@
 echo "Starting unsigned service"
 export WORKER_ROLE=UNSIGNED
-uvicorn sub_services.sub_service:app --port $UNSIGNED_PORT > access_unsigned.log &
+uvicorn sub_services.sub_service:app --port "$UNSIGNED_PORT" > access_unsigned.log &
 
