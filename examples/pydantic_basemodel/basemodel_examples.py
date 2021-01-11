7684eca5934d7bcf420dc0dc56ad72127cd6e109
kkaris <karis.klas@gmail.com>
2021-01-11 09:01:24 -0500

    Add some todos in file docstring

--- examples/pydantic_basemodel/basemodel_examples.py
+++ examples/pydantic_basemodel/basemodel_examples.py
@@ -1,5 +1,12 @@
 """
 Start here: https://pydantic-docs.helpmanual.io/usage/models/
+
+Show:
+- Proper usage of optionals
+- Proper usage of default values
+- Proper way to add methods
+- How to add dynamic attribute names (i.e. model.<name> where `name` can
+  change depending on input
 """
 
 from pydantic import BaseModel
