2738007d48f4c727bd983edffb2ed34a77d6f801
kkaris <karis.klas@gmail.com>
2021-01-19 08:36:51 -0500

    Fix data dir bug

--- examples/run_sub_services.sh
+++ examples/run_sub_services.sh
@@ -1,5 +1,5 @@
 # Set env
-export DATA_DIR=/home/klas/repos/fastapi/examples/data/
+export DATA_DIR=/home/klas/repos/fastapi_test/examples/data/
 export MAIN_PORT=8000
 export SIGNED_PORT=8001
 export UNSIGNED_PORT=8002
@@ -9,4 +9,3 @@ export GRAPH_DIR=/home/klas/repos/depmap_analysis/indra_depmap_service/_cache/
 ./start_unsigned.sh &
 ./start_signed.sh &
 ./start_public_api.sh &
-
