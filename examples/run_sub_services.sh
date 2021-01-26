04a55797f61e321420c7d744a6d4c873346d9262
kkaris <karis.klas@gmail.com>
2021-01-26 09:31:26 -0500

    Update graph dir

---
+++ examples/run_sub_services.sh
@@ -0,0 +1,11 @@
+# Set env
+export DATA_DIR=/home/klas/repos/fastapi_test/examples/data/
+export MAIN_PORT=8000
+export SIGNED_PORT=8001
+export UNSIGNED_PORT=8002
+export GRAPH_DIR=/home/klas/repos/indra_network_search/indra_network_search/_cache/
+
+# Start workers (are they running in separate processes??)
+./start_unsigned.sh &
+./start_signed.sh &
+./start_public_api.sh &
