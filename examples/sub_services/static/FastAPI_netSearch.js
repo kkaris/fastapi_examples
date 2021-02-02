f729dc985d58298eed8af1ad6b5304a4867e54c0
kkaris <karis.klas@gmail.com>
2021-02-02 09:19:53 -0500

    Add query URL to running FastAPI service

--- examples/sub_services/static/FastAPI_netSearch.js
+++ examples/sub_services/static/FastAPI_netSearch.js
@@ -1,4 +1,5 @@
 const SUBMIT_URL = './query/submit';
+const QUERY_URL = 'http://localhost:8000/query';
 const S3_QUERY_CAHCE = 'https://bigmech.s3.amazonaws.com/indra_network_search/';
 const MAX_K_PATHS = 50;
 let pathStmtHashes = [];
